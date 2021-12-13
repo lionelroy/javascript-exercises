c9c745f0324f5bc834ad39d5e39c896f01ed2eb6
Stephane Roy <stephroy411@hotmail.com>
2021-12-13 11:48:12 -0400

    Delete git_test and extracted javascript exercise git repositories

---
+++ palindromes/palindromes.js
@@ -0,0 +1,12 @@
+const palindromes = function(string) {
+    processedString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    return (
+      processedString
+        .split("")
+        .reverse()
+        .join("") == processedString
+    );
+  };
+  
+  module.exports = palindromes;
+  
